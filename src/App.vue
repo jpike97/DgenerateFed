5c6489ea635b2c8221121d7d5b480dbc3276fc38
jpike97 <jpike@sagepath.com>
2020-05-27 11:28:39 -0400

    second commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,38 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Navigation></Navigation>
+    <router-view/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import nav from './components/nav'
 export default {
-  name: 'App',
+  name: 'app',
   components: {
-    HelloWorld
+    'Navigation': nav
   }
 }
 </script>
 
-<style>
+<style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#nav {
+  padding: 30px;
+  a { 
+  font-weight: bold;
+  color: #2c3e50;
+
+  &.router-link-exact-active { 
+    color: #42b983;
+  }
+  }
 }
 </style>
