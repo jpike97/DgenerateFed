20b0395ec5d079c034b4d7fb315c953e661f4713
jpike97 <jpike@sagepath.com>
2020-08-27 15:23:28 -0400

    card grid update

--- client/src/components/cardgrid.vue
+++ client/src/components/cardgrid.vue
@@ -1,56 +1,73 @@
 <template>
-  <div class="cardgrid">
-      <h1>Card Grid Test</h1>
-    <div class="card-test" v-for="card in cards" :key="card.id">
-        {{card.name}}
-    </div>
-    </div>
+	<div class="cardgrid">
+		<h1>Card Grid Test</h1>
+		<div class="card__wrapper" v-for="card in cards" :key="card.id">
+      <div class="card__header">
+       <img src="@/assets/logo.png" />
+       <h2>{{card.ticker}} </h2>
+      </div>
+		</div>
+	</div>
 </template>
 <script>
-import '@/scss/_variables.scss';
-import PostsService from '@/services/CardsService'
+import "@/scss/_variables.scss";
+import PostsService from "@/services/CardsService";
+import jQuery from 'jquery'
+let $ = jQuery
+
+console.log($(".flip-container"));
+
+
 
 export default {
-  name: 'cardgrid',
-  data: function () {
-    return {
-      cards: []
-    }
-  },
-  mounted () {
-    this.getCards()
-  },
-  methods: {
-    async getCards () {
-      const response = await PostsService.fetchCards()
-      this.cards = response.data
-    }
-  }
-}
+	name: "cardgrid",
+	data: function () {
+		return {
+			cards: []
+		};
+	},
+	mounted() {
+    this.getCards();
+    $(document).on("click",".flip-container", function() {
+        console.log(this);
+        $(this).toggleClass('flip-container-clicked');
+    });
+    
+	},
+	methods: {
+		async getCards() {
+			const response = await PostsService.fetchCards();
+			this.cards = response.data;
+		}
+	}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss" scoped>
-@import '@/scss/_variables.scss';
+@import "@/scss/_variables.scss";
 h3 {
-  margin: 40px 0 0;
+	margin: 40px 0 0;
 }
 ul {
-  list-style-type: none;
-  padding: 0;
+	list-style-type: none;
+	padding: 0;
 }
 li {
-  display: inline-block;
-  margin: 0 10px;
+	display: inline-block;
+	margin: 0 10px;
 }
 a {
-  color: #42b983;
+	color: #42b983;
 }
-.card { 
-  &-test { 
-      @media(min-width: $screen-md) { 
-        border: 1xp solid red;
-      }
-  }
+.card {
+	&-test {
+    position: relative;
+		@media (min-width: $screen-md) {
+			border: 1xp solid red;
+		}
+	}
 }
+
+
 </style>
