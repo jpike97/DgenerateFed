b4e223bffa749bd80528218910caa876ca233d44
jpike97 <jpike@sagepath.com>
2020-08-27 15:24:06 -0400

    update home view

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,19 +1,16 @@
 <template>
   <div class="home">
-    <home />
     <cardgrid />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import home from '@/components/home.vue'
 import cardgrid from '@/components/cardgrid.vue'
 
 export default {
   name: 'Home',
   components: {
-    home, 
     cardgrid
   }
 }
