2ae2fc66d980b33dbbd04b17b42f8a9899a33c5a
jpike97 <jpike@sagepath.com>
2020-09-14 15:08:31 -0400

    options update

--- server/options.js
+++ server/options.js
@@ -1,4 +1,4 @@
 var fs = require('fs'),
 configPath = 'config.json';
 var parsed = JSON.parse(fs.readFileSync(configPath, 'UTF-8'));
-exports.storageConfig=  parsed;
+exports.storageConfig =  parsed;
