865bb410e4461f899a64b93c7de27d2f40a3930c
jpike97 <jpike@sagepath.com>
2020-09-20 15:36:16 -0400

    TODO update

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -9,6 +9,7 @@
 
 <script>
 // @ is an alias to /src
+//TODO: get rid of home component doesn't make a lot of sense
 import cardgrid from '@/components/cardgrid.vue'
 import home from '@/components/home.vue'
 
