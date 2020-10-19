201fb735bfbf6705df557a33c54eb3dd701a291e
jpike97 <jpike@sagepath.com>
2020-10-19 16:14:55 -0400

    home page update

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="home">
+    <div class="testing" style="background-color: white;">
+    <home />
+    </div>
     <cardgrid />
   </div>
 </template>
@@ -8,10 +11,13 @@
 // @ is an alias to /src
 //TODO: get rid of home component doesn't make a lot of sense
 import cardgrid from '@/components/cardgrid.vue'
+import home from '@/components/home.vue'
+
 export default {
   name: 'Home',
   components: {
     cardgrid,
+    home
   }
 }
 </script>
