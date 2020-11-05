bd2ba562ffb8e36addf2cb9d07bb3be70d971714
jpike97 <jpike@sagepath.com>
2020-11-05 17:54:57 -0500

    mnobile nav

--- client/src/components/nav.vue
+++ client/src/components/nav.vue
@@ -3,21 +3,32 @@
       <nav class="desktop">
       <router-link to="/">Home</router-link>
       <router-link to="/about">About</router-link>
-      <router-link to="/rando">Rando</router-link>
+      <!-- <router-link to="/rando">Rando</router-link> -->
       </nav>
       <nav class="mobile-tray">
-      <h1>DGENERATE!</h1>
+      <div class="mobile-nav__header">
+      <h1>Dgenerate!</h1>
+      <button class="hamburger hamburger--collapse" type="button">
+      <span class="hamburger-box">
+      <span class="hamburger-inner"></span>
+      </span>
+      </button>
+      </div>
+ 
       <div class="mobile-slideout">
-      <router-link to="/">Home</router-link>
-      <router-link to="/about">About</router-link>
-      <router-link to="/rando">Rando</router-link>
+        <div class="mobile-nav__wrapper">
+        <router-link to="/">Home</router-link>
+        <router-link to="/about">About</router-link>
+      </div>
+      <!-- <router-link to="/rando">Rando</router-link> -->
       </div>
       </nav>
   </div>
 </template>
 
 <script>
-
+import jQuery from "jquery";
+let $ = jQuery;
 export default {
   name: 'home',
   data: function() {
@@ -25,6 +36,12 @@ return {
     avgColor: ''
   };
 },
+	mounted() {
+		$(document).on("click", ".hamburger", function () {
+      $('.hamburger').toggleClass('is-active');
+      $('.mobile-slideout').toggleClass('active');
+		});
+	},
 computed: {
 },
 methods:  {
@@ -35,97 +52,6 @@ methods:  {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss" scoped>
 @import "@/scss/_variables.scss";
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-nav { 
-  padding: 1rem 0rem;
-
-  &.desktop { 
-    display: none;
-    @media (min-width: $screen-lg) { 
-      display: block;
-    }
-  a { 
-    margin-right: 6rem;
-    font-family: 'F37 Ginger',Helvetica,Arial,sans-serif;
-    font-weight: 700;
-    letter-spacing: .3px;
-    position: relative;
-    text-decoration: none;
-    font-size: 1.75rem;
-    color: $white;
-
-
-        &:after {
-          content: '';
-          position: absolute;
-          bottom: -0.5rem;
-          left: 50%;
-          transform: translateX(-50%);
-          width: 0%;
-          height: 2px;
-          background: $white;
-          transition: width 0.25s $ease-in-quad;
-        }
-
-        &:hover,
-        &:active,
-        &:focus,
-        &.active,
-        &.router-link-exact-active {
-          &:after {
-            width: 100%;
-            transition: width 0.25s $ease-out-quad;
-          }
-        }
-
-    &:last-of-type { 
-      margin-right: 0;
-    }
-  }
-}
-&.mobile-tray { 
-  display: block;
-  @media (min-width: $screen-lg) { 
-    display: none;
-  }
-  h1 { 
-    color: $white;
-    margin: 0rem;
-  }
-  .mobile-slideout { 
-    position: absolute;
-    z-index: 1;
-    display: block;
-    width: 100%;
-    height: 100%;
-    top: 0;
-    left: 0;
-    visibility: hidden;
-    padding-top: 4rem;
-    background-color: black;
-    transform: translateX(-100%);
-    transition: transform 0.25s $ease-in-quad, visibility .25s;
-    &.active { 
-      visibility: visible;
-      transform: translateX(0%);
-      transition: transform 0.25s $ease-out-quad, visibility .25s ease 0s;
-      -webkit-overflow-scrolling: touch;
-    }
-  }
-}
-
-}
+@import "@/scss/_nav.scss";
+@import "@/scss/third-party/hamburgers/hamburgers.scss";
 </style>
