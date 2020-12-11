a529a6ff2648cf4edecb368f4e452edd5fe88a71
jpike97 <jpike@sagepath.com>
2020-12-10 22:03:52 -0500

    card grid updates

--- client/src/components/cardgrid.vue
+++ client/src/components/cardgrid.vue
@@ -1,13 +1,18 @@
 <template>
 	<div class="cardgrid">
-		<h1>Card Grid Test</h1>
+		<h1>Stocks to Watch</h1>
 		<div class="cardgrid__cards">
-			<div class="card__wrapper" v-for="card in cards" :key="card.id">
+			<div
+				class="card__wrapper"
+				:class="card.numMentions > 15 ? 'wow' : ''"
+				v-for="card in cards"
+				:key="card.id"
+			>
 				<a :href="'/cards/' + card.ticker" class="card__header">
 					<div class="card__header-info">
 						<h2>{{ card.ticker }}</h2>
 						<h3>{{ card.currentPrice.toFixed(2) }}</h3>
-						<h3>{{ card.numMentions }}</h3>
+						<h3>{{ card.numMentions }} mentions</h3>
 					</div>
 				</a>
 			</div>
@@ -65,6 +70,9 @@ a {
 	text-decoration: none;
 }
 .cardgrid {
+	h1 {
+		margin-top: 20px;
+	}
 	&__cards {
 		display: flex;
 		flex-flow: wrap;
@@ -90,11 +98,45 @@ a {
 		margin: 0px 1rem;
 		margin-bottom: 3rem;
 		cursor: pointer;
-	}
-	&__header { 
-		&-info { 
-			padding: 2rem 0;
+		border-radius: 15px;
+		border: 1px solid #1a1a19;
+		transition: transform 0.25s ease-in-out;
+		&:hover {
+			transform: scale(1.1);
+			transition: transform 0.25s ease-in-out;
 		}
+
+		&.wow {
+			animation: pulse 2s infinite;
+			&:hover { 
+				animation-play-state: paused;
+				border: 2px solid red;
+			}
+		}
+	}
+	&__header {
+		&-info {
+			padding: 2rem 0;
+			h3 {
+			}
+		}
+	}
+}
+
+@keyframes pulse {
+	0% {
+		transform: scale(0.95);
+		box-shadow: 0 0 0 0 red;
+	}
+
+	70% {
+		transform: scale(1);
+		box-shadow: 0 0 0 10px red;
+	}
+
+	100% {
+		transform: scale(0.95);
+		box-shadow: 0 0 0 0 red;
 	}
 }
 </style>
