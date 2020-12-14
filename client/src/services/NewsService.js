7041631f5762d27ea11b760001796d21f7693e47
jpike97 <jpike@sagepath.com>
2020-12-14 01:52:01 -0500

    newsService in progress

--- client/src/services/NewsService.js
+++ client/src/services/NewsService.js
@@ -1,13 +1,19 @@
 import jQuery from "jquery";
 let $ = jQuery;
-//TODO: move this to python logic & put it in a CRON task probably
+
+//TODO: move this to SERVER SIDE and CACHE -- see server.js (separate server.js too prob);
 export default {
     fetchNews (id, callback) {
+    console.log(id);
+    id += "-ticker";
     var feedURL = "https://news.google.com/rss/search?q=" + id;
     $.ajax({
         type: 'GET',
         url: "https://api.rss2json.com/v1/api.json?rss_url=" + feedURL,
         dataType: 'jsonp',
+        data:  { 
+            api_key: "1"
+        },
         success: function(result) {
             console.log("in service");
             console.log(result);
