52e648fe7fbf56ac8994e8cf624a005d268a89bc
jpike97 <jpike@sagepath.com>
2021-01-04 13:13:56 -0500

    news update

--- client/src/services/NewsService.js
+++ client/src/services/NewsService.js
@@ -1,24 +1,7 @@
-import jQuery from "jquery";
-let $ = jQuery;
+import Api from '@/services/Api'
 
-//TODO: move this to SERVER SIDE and CACHE -- see server.js (separate server.js too prob);
 export default {
-    fetchNews (id, callback) {
-    console.log(id);
-    id += "-ticker";
-    var feedURL = "https://news.google.com/rss/search?q=" + id;
-    $.ajax({
-        type: 'GET',
-        url: "https://api.rss2json.com/v1/api.json?rss_url=" + feedURL,
-        dataType: 'jsonp',
-        data:  { 
-            api_key: "1"
-        },
-        success: function(result) {
-            console.log("in service");
-            console.log(result);
-            callback(true, result);
-        }
-      });
+  fetchNewsByID(id) {
+    return Api().get("news/" + id)
   }
 }
