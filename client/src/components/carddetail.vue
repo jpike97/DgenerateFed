c66cf85232200ced5a1124ae71072a6020ba0d05
jpike97 <jpike@sagepath.com>
2021-01-04 13:13:45 -0500

    news update

--- client/src/components/carddetail.vue
+++ client/src/components/carddetail.vue
@@ -8,7 +8,7 @@
 			<h2>News</h2>
 			<div class="news__wrapper" v-for="newsArticle in news" :key="newsArticle.id">
 				<h3>
-				<a target="_blank" :href='newsArticle.link'> {{ newsArticle.title }} </a>
+				<a target="_blank" :href='newsArticle[1]'> {{ newsArticle[0] }} </a>
 				</h3>
 			</div>
 		</div>
@@ -19,27 +19,19 @@ import "@/scss/_variables.scss";
 import CardDetailService from "@/services/CardDetailService";
 import NewsService from "@/services/NewsService";
 //TODO: run ajax after page load? prevent spinny
-import jQuery from "jquery";
-let $ = jQuery;
 
 export default {
 	name: "carddetail",
 	data: function () {
 		return {
 			card: {},
-			news: [],
-			newsLoaded: false
+			news: []
 		};
 	},
 	mounted() {
 		this.getCardDetail();
-		let self = this;
-		//trick to look faster? fade in news?
-		setTimeout(function(){ 
-		console.log("blah");
-		self.getCardNews();
-		self.newsLoaded = true;
-		}, 1500);
+		this.getCardNews();//trick to look faster? fade in news?
+		
 		
 	},
 	methods: {
@@ -53,18 +45,10 @@ export default {
 			this.card.currentPrice = this.card.currentPrice.toFixed(2);
 		},
 		async getCardNews() {
-			let self = this;
 			let cardID = this.$route.params.id;
-			NewsService.fetchNews(cardID, function (result, data) {
-				if (result == true) {
-					console.log("success!");
-					console.log(data.items);
-					self.news = data.items;
-					self.newsLoaded = true;
-					//show the loaded news
-					$('.news').addClass('newsLoaded');
-				}
-			});
+			const response = await NewsService.fetchNewsByID(cardID);
+			this.news = response.data.news;
+			console.log(this.news);
 		}
 	}
 };
@@ -94,12 +78,7 @@ a {
 .card-detail {
 
 	.news { 
-		opacity: 0;
 		padding-top: 40px;
-		&.newsLoaded  { 
-			opacity: 1;
-			transition: opacity .5s ease-in-out;
-		}
 	}
 }
 </style>
