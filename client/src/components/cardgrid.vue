43ea991af41544e88f17ae41522095dd2b827504
jpike97 <jpike@sagepath.com>
2021-01-05 18:22:50 -0500

    no log

--- client/src/components/cardgrid.vue
+++ client/src/components/cardgrid.vue
@@ -42,7 +42,6 @@ export default {
 	methods: {
 		async getCards() {
 			const response = await CardsService.fetchCards();
-			console.log(response);
 			this.cards = response.data.cards.filter(function (card) {
 				return card.currentPrice != null;
 			});
